6f5060360e52b54e270378d8bcd8b2fc91cb785c
omarkshan <omar.shan99@gmail.com>
2021-02-11 12:01:23 +0200

    [srcServer.js]: Adds Webpack configuration and passing it to the app

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,12 +1,20 @@
 "use strict";
-
 import express from "express";
-
 import path from "path";
 import open from "open";
+import webpack from "webpack";
+import config from "../webpack.config.dev";
 
 const port = 3000;
 const server = express();
+const compiler = webpack(config);
+
+server.use(
+  require("webpack-dev-middleware")(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath,
+  })
+);
 
 server.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../src/index.html"));
